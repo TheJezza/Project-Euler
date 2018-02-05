6a023949b61aa921303d30d3bf2d7fb0a217e3f0
TheJezza <exslegend@gmail.com>
2018-02-04 21:00:15 -0800

    A more effecient (but less correct) solution to Problem 3 after doing so research

--- PE003.py
+++ PE003.py
@@ -1,23 +1,12 @@
+import shelve
+
 def compute(limit):
-	factors = primes(limit)
-	#for p in primes:
-	for f in factors:
-		if factors[f]==True and (limit%f) != 0:
-			factors[f] = False
-	return max([i for i in factors if factors[i]==True])
-
-def primes(limit):
-	limitn = limit + 1
-	primes = dict()
-	for i in range(2, limitn):
-		primes[i] = True
-
-	for p in primes:
-		factors = range(p, limitn, p)
-		for f in factors[1:]:
-			primes[f] = False
-	return primes
-	#return [i for i in primes if primes[i]==True]
+	factor = 2
+	while factor*factor < limit:
+		while limit % factor == 0:
+			limit = limit / factor
+		factor += 1
+	return int(limit)
 
 if __name__ == '__main__':
-  print(compute(536))
+	print(compute(600851475143))
